cc045e1788bb983f0fa3d2a29dd0a4c12ca529e4
Administrator <Administrator@zhuyuejun>
2015-10-16 14:34:38 +0800

    创建了市长

---
+++ twoDemo/src/com/pro/domain/Book.java
@@ -0,0 +1,15 @@
+package com.pro.domain;
+
+public class Book {
+	private int bookId;
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	
+	
+}
---
+++ twoDemo/src/com/pro/domain/ShiZhang.java
@@ -0,0 +1,15 @@
+package com.pro.domain;
+
+public class ShiZhang {
+	private int shizhangId;
+
+	public int getShizhangId() {
+		return shizhangId;
+	}
+
+	public void setShizhangId(int shizhangId) {
+		this.shizhangId = shizhangId;
+	}
+	
+	
+}
